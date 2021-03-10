409e98530d1927bf12def6a1d634423fdba4382b
AndyFromTaiwan <chuangp@student.unimelb.edu.au>
2021-03-10 18:25:09 +1100

    update

--- frontend/src/components/add-user.component.js
+++ frontend/src/components/add-user.component.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
 
 export default class AddUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: ''
+    }
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      email: this.state.email
+    }
+
+    console.log(user);
+    //TODO
+
+    this.setState({
+      username: '',
+      email: ''
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the Add User component!</p>
+        <h3>Please register your info here:</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={ e => this.setState({username: e.target.value}) }
+            />
+          </div>
+          <div className="form-group">
+            <label>Email: </label>
+            <input  type="text"
+              required
+              className="form-control"
+              value={this.state.email}
+              onChange={ e => this.setState({email: e.target.value}) }
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Sign Up" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
     );
   }
